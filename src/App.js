1b949fb08017381031b7a90bac456fbac5d7c6b5
josuembuyu <josuembuyu94@gmail.com>
2020-11-30 11:07:55 +0100

    Gestion routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+
+import About from '../src/Component/plateforme/about.jsx'
+import Home from '../src/Component/plateforme/home.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
--- src/Component/plateforme/home.jsx
+++ src/Component/plateforme/home.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class about extends Component {
+export default class home extends Component {
     render() {
         return (
             <div>
-                About page
+                Home page
             </div>
         )
     }
