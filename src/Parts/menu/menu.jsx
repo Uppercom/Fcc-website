243f5ed395e63412c5103bc4eb4d7acf29dc5a50
josuembuyu <josuembuyu94@gmail.com>
2020-12-01 20:49:27 +0100

    Design et responsivité du menu

--- src/Parts/menu/menu.jsx
+++ src/Parts/menu/menu.jsx
@@ -18,25 +18,225 @@ export default class menu extends Component {
                             <div className="nav-dropdown">
                                 <p>
                                     La plateforme
-                                    <img src={chevronDown} alt="Menu" className="nav_chevron" srcset=""/>
+                                    <img src={chevronDown} alt="Menu" className="nav__chevron" srcset=""/>
                                 </p>
                                 <ul className="nav-dropdown__item">
                                     <li>
-                                        <a href="/about">Qui sommes-nous ?</a>
+                                        <a href="/plateforme/a-propos">Qui sommes-nous ?</a>
                                     </li>
                                     <li>
-                                        <a href="/about">Notre organisation</a>
+                                        <a href="/plateforme/organisation">Notre organisation</a>
                                     </li>
                                     <li>
-                                        <a href="/about">Notre fonctionnement</a>
+                                        <a href="/plateforme/fonctionnement">Notre fonctionnement</a>
                                     </li>
                                     <li>
-                                        <a href="/about">Nos députés</a>
+                                        <a href="/plateforme/deputes">Nos députés</a>
+                                    </li>
+                                    <li>
+                                        <a href="/plateforme/senateurs">Nos sénateurs</a>
+                                    </li>
+                                    <li>
+                                        <a href="/plateforme/elus-locaux">Nos élus locaux</a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
+
+                        <div className="l__row">
+                            <div className="nav-dropdown">
+                                <p>
+                                    Actualités
+                                    <img src={chevronDown} alt="Menu" className="nav__chevron" srcset=""/>
+                                </p>
+                                <ul className="nav-dropdown__item">
+                                    <li>
+                                        <a href="/plateforme/a-propos">Nos activités</a>
+                                    </li>
+                                    <li>
+                                        <a href="/plateforme/organisation">InfoCovid</a>
+                                    </li>
+                                    <li>
+                                        <a href="/plateforme/fonctionnement">Toutes nos actualités</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="l__row">
+                            <div className="nav-dropdown">
+                                <p>
+                                    Elections
+                                    <img src={chevronDown} alt="Menu" className="nav__chevron" srcset=""/>
+                                </p>
+                                <ul className="nav-dropdown__item">
+                                    <li>
+                                        <a href="/elections/elections-2023">Elections 2023</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="l__row">
+                            <div className="nav-dropdown">
+                                <p>
+                                    S'engager
+                                    <img src={chevronDown} alt="Menu" className="nav__chevron" srcset=""/>
+                                </p>
+                                <ul className="nav-dropdown__item">
+                                    <li>
+                                        <a href="/s-engager/evenements">Évenements</a>
+                                    </li>
+                                    <li>
+                                        <a href="/s-engager/comites">Comités</a>
+                                    </li>
+                                    <li>
+                                        <a href="/s-engager/parties-politiques">Parties politiques</a>
+                                    </li>
+                                    <li>
+                                        <a href="/s-engager/projets-citoyens">Projets citoyens</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="l__row">
+                            <div className="nav-dropdown">
+                                <p>
+                                    Nos idées
+                                    <img src={chevronDown} alt="Menu" className="nav__chevron" srcset=""/>
+                                </p>
+                                <ul className="nav-dropdown__item">
+                                    <li>
+                                        <a href="/idees/projet">Notre projet</a>
+                                    </li>
+                                    <li>
+                                        <a href="/idees/resultats">Nos résultats concrets</a>
+                                    </li>
+                                    <li>
+                                        <a href="/idees/publications">Nos publications</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <li className="b__nudge--left-medium">
+                            <a className="je-cherche" href="javascript:void(0)">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="23"
+                                height="24"
+                                ariaLabelledby="title"
+                                className="icon-search"
+                                viewBox="0 0 23 24"
+                                >
+                                <path
+                                    fill="#333"
+                                    d="M9.926 0C4.444 0 0 4.444 0 9.926s4.444 9.927 9.926 9.927 9.926-4.445 9.926-9.927C19.852 4.444 15.408 0 9.926 0m0 1c4.922 0 8.926 4.004 8.926 8.926 0 4.922-4.004 8.927-8.926 8.927C5.004 18.853 1 14.848 1 9.926S5.004 1 9.926 1"
+                                ></path>
+                                <path
+                                    stroke="#333"
+                                    strokeLinecap="round"
+                                    d="M16.63 16.709l6 5.927"
+                                    className="stroke"
+                                ></path>
+                            </svg>
+                            </a>
+                        </li>
                     </ul>
+                    <div className="l__row">
+                        <div className="font-roboto b__nudge--right"></div>
+                        <a className="text--body b__nudge--right" href="/adhesion">Adhérer</a>
+                        <a className="btn btn--red" href="/don">Faire un don</a>
+                    </div>
+                </nav>
+
+                <nav className="gtm__navigation__mobile l__wrapper l__hide--on-desktop space--20-0">
+                    <ul className="list__links list__links--row list__links--nav l__row--h-stretch">
+                        <li>
+                            <a href="/" className="link--no-decor text--logo">FCC!</a>
+                        </li>
+                        <li>
+                            <a class="btn btn--red b__nudge--right-extra-large" href="/don">Donner</a>
+                        </li>
+                    </ul>
+                    <div className="nav-mobile">
+                        <input checked type="checkbox" id="nav-menu"/>
+                        <label htmlFor="nav-menu">Menu de navigation</label>
+                        <div className="nav-mobile--menu em-nav em-nav-mobile l__col l__col--top">
+                            <div class="em-nav-mobile__adherent-profile">
+                                <div class="b__nudge--bottom space--20">
+                                    
+                                    <div class="em-nav__menu em-nav__menu--mobile">
+                                        <div class="b__nudge--bottom">
+                                            <ul class="items">
+                                                <li><a href="/connexion">Connexion</a></li>
+                                                <li><a href="/adhesion">Adhérer</a></li>
+                                                <li><a href="https://aide.en-marche.fr" rel="noopener noreferrer" target="_blank">Aide</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="em-nav-mobile__sitemap">
+                                <div class="space--20 b__nudge--bottom">
+                                    <div class="em-nav__menu em-nav__menu--mobile">
+                                        <div class="b__nudge--bottom">
+                                            <span class="head">Le Mouvement</span>
+                                            <ul class="items">
+                                                <li><a href="/le-mouvement">Qui sommes-nous ?</a></li>
+                                                <li><a href="/le-mouvement/notre-organisation">Notre organisation</a></li>
+                                                <li><a href="/le-mouvement/fonctionnement">Notre fonctionnement</a></li>
+                                                <li><a href="/le-mouvement/nos-referents">Nos référents</a></li>
+                                                <li><a href="https://dpt.en-marche.fr" target="_blank" rel="noopener noreferrer">Nos sites départementaux</a></li>
+                                                <li><a href="//legislatives.en-marche.fr/" target="_blank" rel="noopener noreferrer">Nos députés</a></li>
+                                                <li><a href="/nos-senateurs">Nos sénateurs</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="b__nudge--bottom">
+                                            <span class="head">Actualités</span>
+                                            <ul class="items">
+                                                <li><a href="/infocovid">InfoCovid</a></li>
+                                                <li><a href="/articles">Toutes nos actualités</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="b__nudge--bottom">
+                                            <span class="head">Élections</span>
+                                            <ul class="items">
+                                                <li><a href="/municipales">Municipales 2020</a></li>
+                                                <li><a href="/consulaires2020">Consulaires 2020</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="b__nudge--bottom">
+                                            <span class="head">S'engager</span>
+                                            <ul class="items">
+                                                <li><a href="/evenements">Événements</a></li>
+                                                <li><a href="/comites">Comités</a></li>
+                                                <li><a href="/projets-citoyens">Projets citoyens</a></li>
+                                                                    <li><a href="/atelier-des-idees">Atelier des idées</a></li>
+                                                <li><a href="https://mooc.en-marche.fr" target="_blank" rel="noopener noreferrer">Formation</a></li>
+                                                <li><a href="https://mooc.en-marche.fr/sengager-a-la-republique-en-marche" target="_blank" rel="noopener noreferrer">S'engager à LaREM</a></li>
+                                                <li><a href="/le-mouvement/devenez-benevole">Devenez bénévole</a></li>
+                                                <li><a href="/don">Faire un don</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="b__nudge--bottom">
+                                            <span class="head">Nos idées</span>
+                                            <ul class="items">
+                                                <li><a href="/notre-projet">Notre projet</a></li>
+                                                <li><a href="/nos-resultats">Nos résultats concrets</a></li>
+                                                <li><a href="/conferences-debats">Les conférences-débats</a></li>
+                                                <li><a href="/publications">Les publications</a></li>
+                                                <li><a href="/vos-contributions">Vos contributions</a></li>
+                                                <li><a href="/apres-la-crise">Après la crise</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </nav>
             </header>
         )
