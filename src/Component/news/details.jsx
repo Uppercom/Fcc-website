db8eae2198c3a1a214fb7ce05577577d4b6d9dba
josuembuyu <josuembuyu94@gmail.com>
2020-12-09 15:31:04 +0100

    Dynamisation du details d'un article

--- src/Component/news/details.jsx
+++ src/Component/news/details.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import API from '../../Helpers/config.js'
-import { sliceText } from '../../Helpers/functions.js'
+import { sliceText, customDate } from '../../Helpers/functions.js'
 import axios from 'axios'
 import Skeleton from 'react-loading-skeleton'
 import Menu from '../../Parts/menu/menu.jsx' 
@@ -12,6 +12,7 @@ import desintox from '../../Images/DESINTOX2020.jpeg'
 import actu from '../../Images/nos-actualites-visuel.jpeg'
 
 import './post.css'
+import article from './article.jsx'
 
 export default class Newsdetails extends Component {
     constructor() {
@@ -31,7 +32,7 @@ export default class Newsdetails extends Component {
         var id = window.location.pathname.split("/")[2];
         this.setState({ articles : {isLoading : true}})
         axios
-            .get(`${API}/api/admin/news/details/${id}`)
+            .get(`${API}/api/news/details/${id}`)
             .then(
                 (res) => {
                     if (res.data.state) {
@@ -77,61 +78,63 @@ export default class Newsdetails extends Component {
                                     <div className="port">
                                         <div className="btPostContentHolder">
                                             <div className="btPostContentHolder">
-                                                <div className="btArticleHeadline">
-                                                    <header class="bt_bb_headline bt_bb_font_weight_bold bt_bb_size_extralarge bt_bb_superheadline bt_bb_subheadline">
-                                                        <h1>
-                                                            <span class="bt_bb_headline_superheadline">
-                                                                <span class="btArticleCategories">
-                                                                    <Link href="/actualites/5/sante" class="btArticleCategory press">Santé</Link>
-                                                                </span>
-                                                            </span>
-                                                            <span class="bt_bb_headline_content">
-                                                                <span>Veteran’s Day Fundraiser</span>
-                                                            </span>
-                                                        </h1>
-                                                        <div class="bt_bb_headline_subheadline">
-                                                            <span class="btArticleDate">January 10, 2018</span>
+                                                {articles.flag &&
+                                                    <React.Fragment>
+                                                        <div className="btArticleHeadline">
+                                                            <header class="bt_bb_headline bt_bb_font_weight_bold bt_bb_size_extralarge bt_bb_superheadline bt_bb_subheadline">
+                                                                <h1>
+                                                                    <span class="bt_bb_headline_content">
+                                                                        <span>{articles.datas.title}</span>
+                                                                    </span>
+                                                                </h1>
+                                                                <div class="bt_bb_headline_subheadline">
+                                                                    <span class="btArticleDate">{customDate(articles.datas.updatedAt)}</span>
+                                                                </div>
+                                                            </header>
                                                         </div>
-                                                    </header>
-                                                </div>
-
-                                                <div className="btArticleMedia">
-                                                    <div className="btMediaBox">
-                                                        <img src="http://vox-populi.bold-themes.com/party/wp-content/uploads/sites/3/2018/09/post_02-1280x800.jpg" alt="http://vox-populi.bold-themes.com/party/wp-content/uploads/sites/3/2018/09/post_02-1280x800.jpg" />
-                                                    </div>
-                                                </div>
-
-                                                <div class="btArticleContent">
-                                                    <div class="bt_bb_wrapper">
-                                                        <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line. Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
-                                                        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-                                                        <p>Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.&nbsp;Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing procedures for reliable supply chains. Dramatically engage top-line web services vis-a-vis cutting-edge deliverables.&nbsp;Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing. Holistically pontificate installed base portals after maintainable products.</p>
-                                                        <p>Palo santo thundercats fingerstache man braid lomo, hashtag poke forage DIY keytar tilde. Letterpress poke kogi skateboard. Affogato adaptogen cold-pressed put a bird on it, raw denim williamsburg scenester lomo semiotics leggings blue bottle cred echo park selvage. Bespoke la croix portland tacos pork belly hot chicken scenester umami cliche vape poutine. PBR&amp;B pickled wayfarers tilde. Wayfarers biodiesel helvetica yr meh. Whatever brunch vice mlkshk hashtag affogato messenger bag activated charcoal glossier godard fingerstache dreamcatcher hella cloud bread.</p>
-                                                    </div>
-                                                </div>
-
-                                                <div className="btArticleShareEtc">
-                                                    <div className="btTagsColumn"></div>
-                                                    <div className="btShareColumn">
-                                                        <div className="bt_bb_icon btIcoFacebook bt_bb_style_filled bt_bb_size_xsmall bt_bb_shape_circle">
-                                                            <a href="https://www.facebook.com/sharer/sharer.php?u=http://vox-populi.bold-themes.com/party/2018/01/10/veterans-day-fundraiser/" target="_self" title="Share on Facebook" class="bt_bb_icon_holder">
-                                                                <i class="fab fa-facebook-f"></i>
-                                                            </a>
+                                                        <div className="btArticleMedia">
+                                                            <div className="btMediaBox">
+                                                                <img src={articles.datas.cover} alt={articles.datas.cover} />
+                                                            </div>
                                                         </div>
-
-                                                        <div className="bt_bb_icon btIcoTwitter bt_bb_style_filled bt_bb_size_xsmall bt_bb_shape_circle">
-                                                            <a href="https://www.facebook.com/sharer/sharer.php?u=http://vox-populi.bold-themes.com/party/2018/01/10/veterans-day-fundraiser/" target="_self" title="Share on Facebook" class="bt_bb_icon_holder">
-                                                                <i class="fab fa-twitter"></i>
-                                                            </a>
+                                                        <div class="btArticleContent">
+                                                            <div class="bt_bb_wrapper">
+                                                                {articles.datas.content}
+                                                            </div>
                                                         </div>
+                                                        <div className="btArticleShareEtc">
+                                                            <div className="btTagsColumn"></div>
+                                                            <div className="btShareColumn">
+                                                                <div className="bt_bb_icon btIcoFacebook bt_bb_style_filled bt_bb_size_xsmall bt_bb_shape_circle">
+                                                                    <a href="https://www.facebook.com/sharer/sharer.php?u=http://vox-populi.bold-themes.com/party/2018/01/10/veterans-day-fundraiser/" target="_self" title="Share on Facebook" class="bt_bb_icon_holder">
+                                                                        <i class="fab fa-facebook-f"></i>
+                                                                    </a>
+                                                                </div>
 
-                                                        <div className="bt_bb_icon btIcoInstagram bt_bb_style_filled bt_bb_size_xsmall bt_bb_shape_circle">
-                                                            <a href="https://www.facebook.com/sharer/sharer.php?u=http://vox-populi.bold-themes.com/party/2018/01/10/veterans-day-fundraiser/" target="_self" title="Share on Facebook" class="bt_bb_icon_holder">
-                                                                <i class="fab fa-instagram"></i>
-                                                            </a>
+                                                                <div className="bt_bb_icon btIcoTwitter bt_bb_style_filled bt_bb_size_xsmall bt_bb_shape_circle">
+                                                                    <a href="https://www.facebook.com/sharer/sharer.php?u=http://vox-populi.bold-themes.com/party/2018/01/10/veterans-day-fundraiser/" target="_self" title="Share on Facebook" class="bt_bb_icon_holder">
+                                                                        <i class="fab fa-twitter"></i>
+                                                                    </a>
+                                                                </div>
+
+                                                                <div className="bt_bb_icon btIcoInstagram bt_bb_style_filled bt_bb_size_xsmall bt_bb_shape_circle">
+                                                                    <a href="https://www.facebook.com/sharer/sharer.php?u=http://vox-populi.bold-themes.com/party/2018/01/10/veterans-day-fundraiser/" target="_self" title="Share on Facebook" class="bt_bb_icon_holder">
+                                                                        <i class="fab fa-instagram"></i>
+                                                                    </a>
+                                                                </div>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </div>
+                                                    </React.Fragment>
+                                                }
+
+                                                {articles.isLoading &&
+                                                    <React.Fragment>
+                                                         <Skeleton height={20} width={100}/> <br/>
+                                                         <Skeleton height={20} width={600}/> <br/>
+                                                         <Skeleton height={20} width={200}/> <br/>
+                                                         <Skeleton height={450} width={900} />
+                                                    </React.Fragment>
+                                                }
                                             </div>
                                         </div>
                                     </div>
--- src/Helpers/functions.js
+++ src/Helpers/functions.js
@@ -11,4 +11,62 @@ const sliceText = (text, delimit) => {
     }
 }
 
-export { sliceText }
+/**
+ * Modelisation de la date
+ * @param {*} date La date
+ */
+const  customDate = (date) => {
+    var myDate = new Date(date),
+        jour = function () {
+
+            return parseInt(myDate.getDate()) < 10 ? '0' + myDate.getDate() : myDate.getDate();
+        },
+        mois = function () {
+
+            //return myDate.getMonth() + 1 < 10 ? '0' + (myDate.getMonth() + 1) : myDate.getMonth() + 1
+            var month = myDate.getMonth() + 1;
+
+            //La date par rapport à sa nomination
+            switch (month) {
+                case 1:
+                    return 'janvier';
+                case 2:
+                    return 'février';
+                case 3:
+                    return 'mars';
+                case 4:
+                    return 'avril';
+                case 5:
+                    return 'mai';
+                case 6:
+                    return 'juin';
+                case 7:
+                    return 'juillet';
+                case 8:
+                    return 'août';
+                case 9:
+                    return 'septembre';
+                case 10:
+                    return 'octobre';
+                case 11:
+                    return 'novembre';
+                case 12:
+                    return 'décembre';
+                default:
+                    return null;
+            }
+        },
+        heure = function () {
+
+            return myDate.getHours() < 10 ? '0' + myDate.getHours() : myDate.getHours();
+
+        },
+        minute = function () {
+
+            return myDate.getMinutes() < 10 ? '0' + myDate.getMinutes() : myDate.getMinutes();
+
+        };
+
+    return jour() + ' ' + mois() + ' ' + myDate.getFullYear() + ' à ' + heure() + 'h' + minute() + 'min';
+}
+export { sliceText, customDate }
